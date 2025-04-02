3117afc64e3ece61f6e75c49fad578c06928ce72
ebrick <bakedsavant@gmail.com>
2025-04-02 16:15:14 -0400

    finalized with version 2 code

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const maxOfTwoNumbers = (x, y) => {
   
 console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
 
-/*
+/*----------------------------------------------------------------------------------------------------------------
 Exercise 2: isAdult()
 
 Write a function named isAdult. It should take an age (number) 
@@ -29,21 +29,31 @@ Example: isAdult(21) should return 'Adult'.
 Complete the exercise in the space below:
 */
 
+/* attempt 1 */
+// const isAdult = (a) => {
+//     let result;
+//     if (a >= 18) {
+//         result = `${a} is an Adult`;
+//     }
+//     else {
+//         result = `${a} is a Minor`;
+//     }
+//     return result;
+// }
+
+// console.log('Exercise 2 Result:', isAdult(21));
+// console.log('Exercise 2 Result:', isAdult(15));
+
+/* version 2 */
 const isAdult = (a) => {
-    let result;
-    if (a >= 18) {
-        result = `${a} is an Adult`;
-    }
-    else {
-        result = `${a} is a Minor`;
-    }
-    return result;
+    if (a >= 18) return `${a} is an Adult`;
+    return `${a} is a Minor`;
 }
 
 console.log('Exercise 2 Result:', isAdult(21));
 console.log('Exercise 2 Result:', isAdult(15));
 
-/*
+/* ----------------------------------------------------------------------------------------------------------------
 Exercise 3: isCharAVowel()
 
 Write a function named isCharAVowel that takes a single character as 
@@ -81,13 +91,13 @@ console.log('Exercise 3 Result:', isCharAVowel2("a"));
 
 
 /*further refinement from Alara */
-const isCharAVowel3 = (a) => {
-    return "aeiouAEIOU".includes(a);
+const isCharAVowel3 = (character) => {
+    return "aeiouAEIOU".includes(character);
 }
 console.log('Exercise 3 Result:', isCharAVowel3("A"));
 
 
-/*
+/*----------------------------------------------------------------------------------------------------------------
 Exercise 4: generateEmail()
 
 Create a function named generateEmail. It should take two strings: 
@@ -108,7 +118,7 @@ const generateEmail = (a, b) => {
 
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 5: greetUser()
 
 Define a function called greetUser. It should take a name and a 
@@ -129,7 +139,7 @@ const greetUser = (name, time) => {
 
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 6: maxOfThree()
 
 Define a function, maxOfThree. It should accept three numbers 
@@ -140,21 +150,31 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 
+/* first attempt */
+// const maxOfThree = (a, b, c) => {
+//     if (a >= b && a>=c) {
+//         return a;
+//     }
+//     else if (b >= a && b >= c) {
+//         return b;
+//     }
+//     else {
+//         return c;
+//     }
+// }
+
+// console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+/* version 2 */
 const maxOfThree = (a, b, c) => {
-    if (a >= b && a>=c) {
-        return a;
-    }
-    else if (b >= a && b >= c) {
-        return b;
-    }
-    else {
-        return c;
-    }
+    if (a >= b && a >= c) return a;
+    if (b >= a && b >= c) return b;
+    return c;
 }
 
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 7: calculateTip()
 
 Create a function called calculateTip. It should take two arguments: 
@@ -167,14 +187,14 @@ Complete the exercise in the space below:
 */
 
 const calculateTip = (bill, tipamount) => {
-    // tip = ((tipamount / 100) * bill);
+    // tip = ((tipamount / 100) * bill); 
     // return tip;
     return ((tipamount / 100) * bill);
 }
 
 console.log('Exercise 7 Result: The tip will cost $' + calculateTip(50, 20));
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 8: convertTemperature()
 
 Write a function named convertTemperature. 
@@ -188,24 +208,36 @@ Example: convertTemperature(32, 'F') should return 0 (Celsius).
 Complete the exercise in the space below:
 */
 
+/* attempt 1 */
+// const convertTemperature = (temp, scale) => {
+//     let temperature;
+//     if (scale === "F") {
+//         temperature = "Temp in F is " + ((temp - 32) * (5/9));
+//     }
+//     else if (scale === "C") {
+//         temperature = "Temp in C is " + ((temp * (9/5)) + 32);
+//     }
+//     else {
+//         temperature = "error in scale format";
+//     }
+//     return temperature;
+// }
+
+// console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+// console.log('Exercise 8 Result:', convertTemperature(32, "F"));
+
+/* version 2 */
 const convertTemperature = (temp, scale) => {
-    let temperature;
-    if (scale === "F") {
-        temperature = "Temp in F is " + ((temp - 32) * (5/9));
-    }
-    else if (scale === "C") {
-        temperature = "Temp in C is " + ((temp * (9/5)) + 32);
-    }
-    else {
-        temperature = "error in scale format";
-    }
-    return temperature;
+    if (scale === "F") return "Temp in F is " + ((temp - 32) * (5/9));
+    if (scale === "C") return "Temp in C is " + ((temp * (9/5)) + 32);
+    return "error in scale format";
 }
 
 console.log('Exercise 8 Result:', convertTemperature(32, "C"));
 console.log('Exercise 8 Result:', convertTemperature(32, "F"));
+console.log('Exercise 8 Result:', convertTemperature(32, "f"));
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 9: basicCalculator()
 
 Create a function named basicCalculator. 
@@ -221,24 +253,41 @@ Example: basicCalculator(10, 5, 'subtract') should return 5.
 Complete the exercise in the space below:
 */
 
+/* attempt 1 */
+// const basicCalculator = (num1, num2, operation) => {
+//     let value;
+//     if (operation === "add") {
+//         value = (num1 + num2);
+//     }
+//     else if (operation === "subtract") {
+//         value = (num1 - num2);
+//     }
+//     else if (operation === "multiply") {
+//         value = (num1 * num2);
+//     }
+//     else if (operation === "divide") {
+//         value = (num1 / num2);
+//     }
+//     else {
+//         value = "error in operation syntax";
+//     }
+//     return value;
+// }
+
+// console.log('Exercise 9 Result:', basicCalculator(10, 5, "add"));
+// console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+// console.log('Exercise 9 Result:', basicCalculator(10, 5, "multiply"));
+// console.log('Exercise 9 Result:', basicCalculator(10, 5, "divide"));
+// console.log('Exercise 9 Result:', basicCalculator(10, 5, "error"));
+
+/* version 2 */
 const basicCalculator = (num1, num2, operation) => {
     let value;
-    if (operation === "add") {
-        value = (num1 + num2);
-    }
-    else if (operation === "subtract") {
-        value = (num1 - num2);
-    }
-    else if (operation === "multiply") {
-        value = (num1 * num2);
-    }
-    else if (operation === "divide") {
-        value = (num1 / num2);
-    }
-    else {
-        value = "error in operation syntax";
-    }
-    return value;
+    if (operation === "add") return (num1 + num2);
+    if (operation === "subtract") return (num1 - num2);
+    if (operation === "multiply") return (num1 * num2);
+    if (operation === "divide") return (num1 / num2);
+    return "error in operation syntax";
 }
 
 console.log('Exercise 9 Result:', basicCalculator(10, 5, "add"));
@@ -247,7 +296,7 @@ console.log('Exercise 9 Result:', basicCalculator(10, 5, "multiply"));
 console.log('Exercise 9 Result:', basicCalculator(10, 5, "divide"));
 console.log('Exercise 9 Result:', basicCalculator(10, 5, "error"));
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 10: calculateGrade()
 
 Define a function called calculateGrade. 
@@ -262,30 +311,44 @@ Example: calculateGrade(100) should return A.
 Complete the exercise in the space below:
 */
 
+/* first attempt */
+// const calculateGrade = (grade) => {
+//     if (grade >= 90) {
+//         grade = "You got an A, Congrats!"
+//     }
+//     else if (grade >= 80) {
+//         grade = "B";
+//     }
+//     else if (grade >= 70) {
+//         grade = "C";
+//     }
+//     else if (grade >= 60) {
+//         grade = "D";
+//     }
+//     else {
+//         grade = "You got an F, study more!";
+//     }
+//     return grade;
+// }
+
+// console.log('Exercise 10 Result:', calculateGrade(85));
+// console.log('Exercise 10 Result:', calculateGrade(95));
+// console.log('Exercise 10 Result:', calculateGrade(55));
+
+/* version 2 */
 const calculateGrade = (grade) => {
-    if (grade >= 90) {
-        grade = "You got an A, Congrats!"
-    }
-    else if (grade >= 80) {
-        grade = "B";
-    }
-    else if (grade >= 70) {
-        grade = "C";
-    }
-    else if (grade >= 60) {
-        grade = "D";
-    }
-    else {
-        grade = "You got an F, study more!";
-    }
-    return grade;
-}
+    if (grade >= 90) return "You got an A, Congrats!";
+    if (grade >= 80) return "B";
+    if (grade >= 70) return "C";
+    if (grade >= 60) return "D";
+    return "You got an F, study more!";
+};
 
-console.log('Exercise 10 Result:', calculateGrade(85));
-console.log('Exercise 10 Result:', calculateGrade(95));
-console.log('Exercise 10 Result:', calculateGrade(55));
+console.log('Exercise 10 Result:', calculateGrade(85)); // "B"
+console.log('Exercise 10 Result:', calculateGrade(95)); // "You got an A, Congrats!"
+console.log('Exercise 10 Result:', calculateGrade(55)); // "You got an F, study more!";
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 11: createUsername()
 
 Define a function called createUsername. 
@@ -302,14 +365,12 @@ Complete the exercise in the space below:
 */
 
 const createUsername = (first, second) => {
-    // let userName;
     return first.slice(0, 3) + second.slice(0, 3) + (first.length + second.length)
-    // return userName;
 }
 
 console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
 
-/*
+/*----------------------------------------------------------------------------------------------------------
 Exercise 12: numArgs()
 
 Challenge yourself with numArgs. 
@@ -323,4 +384,3 @@ const numArgs = (...args) => {
 }
 
 console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
-
